6b104c45de1c4bdff60e7fbbe2432f7966f2ae53
Kbza <g.leonel.m17@gmail.com>
2023-08-01 13:52:05 -0300

    0.11

--- src/app/main/views/components/home/home.component.ts
+++ src/app/main/views/components/home/home.component.ts
@@ -43,7 +43,7 @@ export class HomeComponent implements OnInit {
 	  console.log('audio is ready')
 
     // setInterval(() => console.log(Tone.now()), 1000);
-    })
+    }) 
 
     // Comienza el transporte de Tone.js
     Tone.Transport.start();
